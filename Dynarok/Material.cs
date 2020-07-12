1b54c2a92eb5ba2e8dbe675383f7b89913d3b56b
Francisco Possetto <franciscopossetto@hotmail.com>
2020-07-12 19:29:33 -0300

    Add get identity properties in Material.cs

--- Dynarok/Material.cs
+++ Dynarok/Material.cs
@@ -2,38 +2,36 @@
 using System.Collections.Generic;
 using Dynamo.Graph.Nodes;
 using Autodesk.Revit.DB;
+using Autodesk.DesignScript.Runtime;
 using Revit.Elements;
 using RevitServices.Persistence;
 using RevitServices.Transactions;
 using System.Linq;
-
 using DB = Autodesk.Revit.DB;
-
-namespace Dynarok
+using RevitElements = Revit.Elements;
+namespace Revit
 {
     public class Material
     {
         private Material() { }
 
         [NodeCategory("Create")]
-        public static Revit.Elements.Element Create(string name)
+        public static RevitElements.Element Create(string name)
         {
             Autodesk.Revit.DB.Document doc = DocumentManager.Instance.CurrentDBDocument;
             if (Exists(name))
                 return null;
 
             ElementId materialId = null;
-            using (Transaction t = new Transaction(doc, "Create Material"))
-            {
-                t.Start();
-                materialId = DB.Material.Create(doc, name);
-                t.Commit();
-            }
+
+            TransactionManager.Instance.EnsureInTransaction(doc);
+            materialId = DB.Material.Create(doc, name);
+            TransactionManager.Instance.TransactionTaskDone();
 
             return doc.GetElement(materialId).ToDSType(true);
         }
 
-        [NodeCategory("Actions")]
+        [NodeCategory("Query")]
         public static bool Exists(string name)
         {
             Autodesk.Revit.DB.Document doc = DocumentManager.Instance.CurrentDBDocument;
@@ -43,37 +41,38 @@ namespace Dynarok
             return materials.Any(x => x.Name == name);
         }
 
+        [NodeCategory("Query")]
+        [MultiReturn(new[] { "Name", "Class" })]
+        public static Dictionary<string, string> GetIdentityProperties(Revit.Elements.Material material)
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+            Autodesk.Revit.DB.Document doc = DocumentManager.Instance.CurrentDBDocument;
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+
+            result["Name"] = material.Name;
+            result["Class"] = material.MaterialClass;
+            
+
+            return result;
+        }
+
         [NodeCategory("Actions")]
-        public static Revit.Elements.Element SetProperties(Revit.Elements.Material material, string name)
+        public static RevitElements.Element SetIdentityProperties(RevitElements.Material material, string name = "", string @class= "")
         {
 
-            Autodesk.Revit.DB.Document doc = DocumentManager.Instance.CurrentDBDocument;
-            Autodesk.Revit.DB.Material mat = (DB.Material) material.InternalElement;
+            DB.Document doc = DocumentManager.Instance.CurrentDBDocument;
+            DB.Material mat = (DB.Material) material.InternalElement;
 
-            using (Transaction t = new Transaction(doc, "Set properties in material"))
-            {
-                t.Start();
-                    if (name != null) mat.Name = name;
-                t.Commit();
-            }
+            TransactionManager.Instance.EnsureInTransaction(doc);
+
+            if (name != null && name != "") mat.Name = name;
+            if ( @class != null) mat.MaterialClass = @class;
+
+            TransactionManager.Instance.TransactionTaskDone();
+
             return mat.ToDSType(true);
         }
 
 
-        //public static Revit.Elements.Element Duplicate(Revit.Elements.Material material, string name)
-        //{
-        //    //material.
-        //    //DB.Material newMaterial = material.Duplicate(name);
-        //    //if (newMaterial != null) return newMaterial;
-        //    //while (newMaterial == null)
-        //    //{
-        //    //    int n = 1;
-        //    //    name = name + "Copy" + " " + name.ToString();
-        //    //    newMaterial = material.Duplicate(name);
-        //    //    n++;
-        //    //}
-        //    //return newMaterial.ToDSType(true); ;
-
-        //}
     }
 }
