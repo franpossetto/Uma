557601e9a6e15eb574803f01f8f8fc9a26533be2
Francisco Possetto <franciscopossetto@hotmail.com>
2020-10-04 21:01:03 -0300

    -Add set tint nodes

--- Dynarok/Asset.cs
+++ Dynarok/Asset.cs
@@ -9,6 +9,9 @@ using RevitServices.Transactions;
 using System.Linq;
 using DB = Autodesk.Revit.DB;
 using RevitElements = Revit.Elements;
+using Autodesk.Revit.DB.Visual;
+using Dynamo.Core;
+using Autodesk.DesignScript.Geometry;
 
 namespace Revit
 {
@@ -47,5 +50,80 @@ namespace Revit
             return materials.Any(x => x.Name == name);
         }
 
+        [NodeCategory("Action")]
+        public static List<string> SetTintColor(Revit.Elements.Element AppearanceAsset, DSCore.Color color)
+        {
+            
+            List<string> output = new List<string>();
+
+            DB.AppearanceAssetElement _appearanceAsset = (DB.AppearanceAssetElement)AppearanceAsset.InternalElement;
+            Autodesk.Revit.DB.Document doc = DocumentManager.Instance.CurrentDBDocument;
+            TransactionManager.Instance.EnsureInTransaction(doc);
+            AppearanceAssetEditScope editScope = new Autodesk.Revit.DB.Visual.AppearanceAssetEditScope(doc);
+            Asset editableAsset = editScope.Start(_appearanceAsset.Id);
+            
+            try
+            {
+            ElementId aid = _appearanceAsset.Id;
+
+            AssetPropertyDoubleArray4d tintColorProp = editableAsset.FindByName("common_Tint_color") as AssetPropertyDoubleArray4d;
+
+            byte r = color.Red;
+            byte g = color.Green;
+            byte b = color.Blue;
+
+            Color _color = new Color(r, g, b);
+           
+            if(tintColorProp.IsEditable()) tintColorProp.SetValueAsColor(_color);
+
+            }
+            catch(Exception e)
+            {
+                output.Add(e.ToString());
+            }
+
+            editScope.Commit(false);
+            TransactionManager.Instance.TransactionTaskDone();
+
+            return output;
+        }
+
+        [NodeCategory("Action")]
+        public static List<string> SetTintColorByMaterial (Revit.Elements.Element Material, DSCore.Color color)
+        {
+
+            List<string> output = new List<string>();
+
+            DB.Material material = (DB.Material)Material.InternalElement;
+            Autodesk.Revit.DB.Document doc = DocumentManager.Instance.CurrentDBDocument;
+            TransactionManager.Instance.EnsureInTransaction(doc);
+            ElementId _appearanceAssetId = material.AppearanceAssetId;
+            if (_appearanceAssetId.IntegerValue.Equals(-1)) return output;
+            AppearanceAssetEditScope editScope = new Autodesk.Revit.DB.Visual.AppearanceAssetEditScope(doc);
+            Asset editableAsset = editScope.Start(_appearanceAssetId);
+
+            try
+            {
+                AssetPropertyDoubleArray4d tintColorProp = editableAsset.FindByName("common_Tint_color") as AssetPropertyDoubleArray4d;
+
+                byte r = color.Red;
+                byte g = color.Green;
+                byte b = color.Blue;
+
+                Color _color = new Color(r, g, b);
+
+                if (tintColorProp.IsEditable()) tintColorProp.SetValueAsColor(_color);
+
+            }
+            catch (Exception e)
+            {
+                output.Add(e.ToString());
+            }
+
+            editScope.Commit(false);
+            TransactionManager.Instance.TransactionTaskDone();
+
+            return output;
+        }
     }
 }
